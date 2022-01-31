9fa3295e8fb8572a0d8315899af6f4a9f5c00921
ruarq <anton@green-pr.org>
2022-01-31 16:53:29 +0100

    added `ValueArray` to `Chunk`

--- source/chunk.c
+++ source/chunk.c
@@ -6,6 +6,8 @@ void chunk_init(Chunk *chunk)
 	chunk->capacity = 0;
 	chunk->code = NULL;
 	chunk_realloc(chunk, 1);
+
+	value_array_init(&chunk->constants);
 }
 
 void chunk_write(Chunk *chunk, const uint8_t byte)
@@ -18,6 +20,11 @@ void chunk_write(Chunk *chunk, const uint8_t byte)
 	chunk->code[chunk->size++] = byte;
 }
 
+void chunk_write_value(Chunk *chunk, const Value value)
+{
+	value_array_write(&chunk->constants, value);
+}
+
 void chunk_realloc(Chunk *chunk, const size_t capacity)
 {
 	chunk->code = realloc(chunk->code, capacity * sizeof(uint8_t));
@@ -26,6 +33,8 @@ void chunk_realloc(Chunk *chunk, const size_t capacity)
 
 void chunk_free(Chunk *chunk)
 {
+	value_array_free(&chunk->constants);
+
 	free(chunk->code);
 	chunk_init(chunk);
 }
--- source/chunk.h
+++ source/chunk.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include "common.h"
+#include "value.h"
 
 typedef enum
 {
@@ -15,10 +16,13 @@ typedef struct
 	size_t size;
 	size_t capacity;
 	uint8_t *code;
+
+	ValueArray constants;
 } Chunk;
 
 void chunk_init(Chunk *chunk);
 void chunk_write(Chunk *chunk, const uint8_t byte);
+void chunk_write_value(Chunk *chunk, const Value value);
 void chunk_realloc(Chunk *chunk, const size_t capacity);
 void chunk_free(Chunk *chunk);
 
