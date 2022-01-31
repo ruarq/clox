f7a022d42d95b6362e106a86a11cef89b7dcc3e6
ruarq <anton@green-pr.org>
2022-01-31 15:44:49 +0100

    added `chunk_write` func

--- source/chunk.c
+++ source/chunk.c
@@ -2,7 +2,26 @@
 
 void chunk_init(Chunk *chunk)
 {
-	chunk->count = 0;
+	chunk->size = 0;
 	chunk->capacity = 0;
-	chunk->code = NULL;
+	chunk_realloc(chunk, 1);
+}
+
+void chunk_write(Chunk *chunk, const uint8_t byte)
+{
+	if (chunk->size >= chunk->capacity)
+	{
+		chunk_realloc(chunk, chunk->capacity * 2);
+	}
+
+	chunk->code[chunk->size++] = byte;
+}
+
+void chunk_realloc(Chunk *chunk, const size_t capacity)
+{
+	// reallocate buffer
+	chunk->code = realloc(chunk->code, capacity * sizeof(uint8_t));
+
+	// assign new capacity
+	chunk->capacity = capacity;
 }
--- source/chunk.h
+++ source/chunk.h
@@ -1,6 +1,8 @@
 #ifndef CLOX_CHUNK_H
 #define CLOX_CHUNK_H
 
+#include <stdlib.h>
+
 #include "common.h"
 
 typedef enum
@@ -10,11 +12,13 @@ typedef enum
 
 typedef struct
 {
-	size_t count;
+	size_t size;
 	size_t capacity;
 	uint8_t *code;
 } Chunk;
 
 void chunk_init(Chunk *chunk);
+void chunk_write(Chunk *chunk, const uint8_t byte);
+void chunk_realloc(Chunk *chunk, const size_t capacity);
 
 #endif
