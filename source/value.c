0743b7989a5c545dedd16aeca1a533e5d3ff6378
ruarq <anton@green-pr.org>
2022-01-31 16:49:28 +0100

    added `ValueArray`

---
+++ source/value.c
@@ -0,0 +1,31 @@
+#include "value.h"
+
+void value_array_init(ValueArray *va)
+{
+	va->size = 0;
+	va->capacity = 0;
+	va->values = NULL;
+	value_array_realloc(va, 1);
+}
+
+void value_array_write(ValueArray *va, const Value value)
+{
+	if (va->size >= va->capacity)
+	{
+		value_array_realloc(va, va->capacity * 2);
+	}
+
+	va->values[va->size++] = value;
+}
+
+void value_array_realloc(ValueArray *va, const size_t capacity)
+{
+	va->values = realloc(va->values, capacity * sizeof(Value));
+	va->capacity = capacity;
+}
+
+void value_array_free(ValueArray *va)
+{
+	free(va->values);
+	value_array_init(va);
+}
--- source/value.h
+++ source/value.h
@@ -1,6 +1,22 @@
 #ifndef CLOX_VALUE_H
 #define CLOX_VALUE_H
 
+#include <stdlib.h>
+
+#include "common.h"
+
 typedef double Value;
+
+typedef struct
+{
+	size_t size;
+	size_t capacity;
+	Value *values;
+} ValueArray;
+
+void value_array_init(ValueArray *va);
+void value_array_write(ValueArray *va, const Value value);
+void value_array_realloc(ValueArray *va, const size_t capacity);
+void value_array_free(ValueArray *va);
 
 #endif
